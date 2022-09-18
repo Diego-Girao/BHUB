24f74f7d7411432cf83369f9fa9e3aa8d621bd08
Diego Ramiro <diegoramiro25@gmail.com>
2022-09-18 02:52:05 -0300

    feature: servidor criado.

---
+++ servidor.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const appRouter = require('./router');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+appRouter.configure(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('Server running on port 🚀 ' + PORT));
